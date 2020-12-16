3026aa823f7dc17fc803d617fc8c3253b501e67b
Ajinkya <42488697+nanawareajinkya@users.noreply.github.com>
2020-12-16 19:25:46 +0530

    file created

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export { default as AddCommentForm } from './AddCommentForm/AddCommentForm';
+export { default as Card } from './Card/Card';
+export { default as CommentCard } from './CommentCard/CommentCard';
+export { default as PostForm } from './PostForm/PostForm';
+export { default as PostList } from './PostList/PostList';
+export { default as PostView } from './PostView/PostView';
+export { default as SideNav } from './SideNav/SideNav';
+export { default as VoteScore } from './VoteScore/VoteScore';
