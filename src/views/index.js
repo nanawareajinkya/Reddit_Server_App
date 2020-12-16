2f67aeb8c1da67b9808dbfe84e3fcd73cc9e459e
Ajinkya <42488697+nanawareajinkya@users.noreply.github.com>
2020-12-16 19:31:23 +0530

    Add files via upload

---
+++ src/views/index.js
@@ -0,0 +1,5 @@
+export { default as App } from './App/App';
+export { default as Home } from './Home/Home';
+export { default as CategoryView } from './CategoryView/CategoryView';
+export { default as ViewPost } from './ViewPost/ViewPost';
+export { default as AddEditPost } from './AddEditPost/AddEditPost';
